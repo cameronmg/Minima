047b33ffe1c2b3a748c954321bbbcafa310e2a1a
cameronmg <camgonza2002@gmail.com>
2022-12-12 23:03:51 -0600

    Styled third component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import background from '../assets/img/color-sharp2.png'
+import iphones from '../assets/img/two-iphone.png'
 
 const Mission = () => {
   return (
@@ -9,11 +10,12 @@ const Mission = () => {
             <div className = "text-white flex justify-center ">
               <div className="w-[280px] md:w-[700px] h-[400px] bg-gradient-to-r from-[#28144c] to-[#3f1b3a] z-0 mt-[50%] md:mt-[15%] text-white rounded-xl">
                 <h1 className = "flex justify-center font-3xl font-bold pt-4 ">What is Minima?</h1>
-                <p className ="text-center pt-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                <p className ="text-center mt-3 md:mt-5 lg:mt-5 px-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
                     Ullam, id nisi, doloribus cum iusto ratione,
                     unde consequatur alias non eligendi dolorum ad voluptas voluptate. 
-                    Possimus esse voluptates eius quae eligendi.</p>
-
+                    Possimus esse voluptates eius quae eligendi.
+                </p>
+              <img src={iphones} className="mx-auto z-0 w-[250px] md:w-[390px] lg:w-[390px]"/>
               </div>
             </div>
         </div>
